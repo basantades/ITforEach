70fbb425715296bc2cfb3d846acfccd5e7f91456
Alberto Basanta <dasantades@gmail.com>
2025-04-09 19:29:49 +0200

    css update

---
+++ src/app/components/blocks/aurora-bg/aurora-bg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aurora-bg',
+  imports: [],
+  templateUrl: './aurora-bg.component.html',
+  styleUrl: './aurora-bg.component.scss'
+})
+export class AuroraBgComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { LoginButtonComponent } from "../../components/ui/login-button/login-button.component";
+import { AuroraBgComponent } from "../../components/blocks/aurora-bg/aurora-bg.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, LoginButtonComponent],
+  imports: [RouterLink, LoginButtonComponent, AuroraBgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

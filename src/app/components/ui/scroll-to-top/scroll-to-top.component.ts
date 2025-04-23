aa2cd49573638c54e05e8a9a91fc090904efec93
Alberto Basanta <dasantades@gmail.com>
2025-04-23 11:57:47 +0200

    bugfix

---
+++ src/app/components/ui/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scroll-to-top.component.html'
+})
+export class ScrollToTopComponent {
+  isVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isVisible = window.scrollY > 200;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
